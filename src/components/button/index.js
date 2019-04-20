310e8195a3b27c4365180b81d014e36905083bd5
Rahul Ballal <rahulballal@gmail.com>
2019-04-20 16:47:42 +1000

    chore: adds emotion for styled-components;

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const StyledButton = styled.button`
+  color: blue;
+  height: 50px;
+`
 
 export const ButtonBase = ({ testId, children, ...others }) => (
-  <button data-testid={testId} {...others}>
+  <StyledButton data-testid={testId} {...others}>
     {children}
-  </button>
+  </StyledButton>
 )
