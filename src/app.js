70d314f4dce972b622463e487c04d869f7b892ad
Rahul Ballal <rahulballal@gmail.com>
2019-04-20 17:00:17 +1000

    chore: configure prettier

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { hydrate } from 'react-dom'
 import { ButtonBase } from './components/button'
 
-const App = () => <div><ButtonBase>Hello World</ButtonBase></div>
+const App = () => (
+  <div>
+    <ButtonBase>Hello World</ButtonBase>
+  </div>
+)
 
 hydrate(<App />, document.querySelector('#app'))
